d627c51ff70043044cc65fecfb1df3c63812e0bd
ravinder <ravinder.chinthpalli@gmail.com>
2019-01-20 21:25:49 -0500

    RenameFileName

--- src/main/java/com/example/demo/JsonRemoveEmptyNode.java
+++ src/main/java/com/example/demo/JsonRemoveEmptyNode.java
@@ -7,7 +7,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 
-public class SampleMain {
+public class JsonRemoveEmptyNode {
 
 	public static void main(String[] args) throws IOException {
 
